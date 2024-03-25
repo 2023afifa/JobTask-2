962819f393b3ec09d2980dc2101f06e8e0763209
afifa <2023afifa@gmail.com>
2024-03-25 21:35:12 +0600

    Sidebar section done for small screen

--- src/app/page.js
+++ src/app/page.js
@@ -14,8 +14,13 @@ const Home = () => {
   return (
 
     <div className="flex">
-      <div className={`h-screen w-64 bg-blue-900 text-gray-200 p-4 transform fixed left-0 top-0 ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
-        <p className='text-gray-200'>Hello User</p>
+      <div className={`h-screen w-64 bg-blue-900 text-gray-200 p-4 transform fixed left-0 top-0 ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-10`}>
+        <div>
+          <p className='text-gray-200'>Hello User</p>
+          <hr></hr>
+          <h2 className='mt-10 text-lg font-semibold'>Discussion Forum</h2>
+          <h2 className='my-3 text-lg font-semibold'>Market Stories</h2>
+        </div>
       </div>
 
       <div className='hidden lg:block'>
@@ -32,9 +37,9 @@ const Home = () => {
       </div>
 
       <div className='block lg:hidden'>
-        <div className={`transition-all ml-0 duration-300 ease-in-out`}>
+        <div className="relative">
           <div className='flex items-center'>
-            <div className='bg-blue-900 h-20 transition-all ml-64 duration-300 ease-in-out'>
+            <div className={`flex-1 transition-all ${isOpen ? 'ml-64' : 'ml-0'} duration-300 ease-in-out bg-blue-900 h-20 absolute`}>
               <button className="w-auto h-20" onClick={toggleSidebar}>
                 <BiSolidRightArrow className='text-gray-200' />
               </button>
