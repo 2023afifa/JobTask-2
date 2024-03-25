8577fdefe8580b764894144df79475e5bc023007
afifa <2023afifa@gmail.com>
2024-03-25 15:37:19 +0600

    Arrow section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { IoMdArrowDroprightCircle } from "react-icons/io";
+import { BiSolidRightArrow } from "react-icons/bi";
 import MainSection from './Components/MainSection';
 
 const Home = () => {
@@ -14,16 +14,17 @@ const Home = () => {
   return (
 
     <div className="flex">
-      <div className={`h-screen w-64 bg-blue-800 text-white p-4 transform fixed left-0 top-0 ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
+      <div className={`h-screen w-64 bg-blue-900 text-gray-200 p-4 transform fixed left-0 top-0 ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
         <p className='text-gray-200'>Hello User</p>
       </div>
 
-      <div className={`flex-1 p-4 transition-all ${isOpen ? 'ml-64' : 'ml-0'} duration-300 ease-in-out`}>
-        <button className="" onClick={toggleSidebar}>
-          <IoMdArrowDroprightCircle className='text-blue-800 text-3xl ml-3' />
-        </button>
-
-        <div className=''>
+      <div className={`flex-1 transition-all ${isOpen ? 'ml-64' : 'ml-0'} duration-300 ease-in-out`}>
+        <div className='flex items-center'>
+          <div className='bg-blue-900 h-20'>
+            <button className="w-auto h-20" onClick={toggleSidebar}>
+              <BiSolidRightArrow className='text-gray-200' />
+            </button>
+          </div>
           <MainSection />
         </div>
       </div>
