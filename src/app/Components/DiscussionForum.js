fcb56b335efa6fb07135387568698791c7f152f1
afifa <2023afifa@gmail.com>
2024-03-25 16:11:16 +0600

    Main section cards responsiveness done

--- src/app/Components/DiscussionForum.js
+++ src/app/Components/DiscussionForum.js
@@ -11,37 +11,37 @@ const DiscussionForum = () => {
             {
                 discussData.map(post => (<div key={post.id}>
 
-                    <div className='mx-5 p-5 shadow-lg flex'>
+                    <div className='mx-2 lg:mx-5 p-5 shadow-lg flex'>
                         <div className='w-1/12'>
-                            <img className='w-12 rounded-full' src={post.image} />
+                            <img className='w-6 lg:w-12 rounded-full' src={post.image} />
                         </div>
                         <div className='w-10/12'>
                             <div className='flex mb-3'>
                                 <h3 className='font-bold mr-5'>{post.name}</h3>
                                 <p className='bg-blue-600 text-gray-200 text-xs font-bold py-1 px-2 rounded-xl'>Sector 1</p>
                             </div>
-                            <p className='text-sm font-medium mb-3'>{post.content}</p>
+                            <p className='text-xs lg:text-sm text-justify font-medium mb-3'>{post.content}</p>
                             <div className='flex justify-evenly'>
                                 <div className='flex items-center'>
-                                    <FaRegHeart className='text-lg mr-1' />
-                                    <p className='text-sm'>2k</p>
+                                    <FaRegHeart className='text-sm lg:text-lg mr-1' />
+                                    <p className='text-xs lg:text-sm'>2k</p>
                                 </div>
                                 <div className='flex items-center'>
-                                    <IoEyeOutline className='text-lg mr-1' />
-                                    <p className='text-sm'>8k</p>
+                                    <IoEyeOutline className='text-sm lg:text-lg mr-1' />
+                                    <p className='text-xs lg:text-sm'>8k</p>
                                 </div>
                                 <div className='flex items-center'>
-                                    <FaRegCommentAlt className='text-lg mr-1' />
-                                    <p className='text-sm'>1k Comments</p>
+                                    <FaRegCommentAlt className='text-sm lg:text-lg mr-1' />
+                                    <p className='text-xs lg:text-sm'>1k Comments</p>
                                 </div>
                                 <div className='flex items-center'>
-                                    <IoShareSocialOutline className='text-lg mr-1' />
-                                    <p className='text-sm'>Share</p>
+                                    <IoShareSocialOutline className='text-sm lg:text-lg mr-1' />
+                                    <p className='text-xs lg:text-sm'>Share</p>
                                 </div>
                             </div>
                         </div>
                         <div className='w-1/12'>
-                            <p className='text-blue-700 text-sm font-semibold'>2 mins ago</p>
+                            <p className='text-blue-700 text-xs font-semibold ml-1'>2 mins ago</p>
                         </div>
                     </div>
                 </div>
--- src/app/Components/MainSection.js
+++ src/app/Components/MainSection.js
@@ -4,14 +4,18 @@ import MarketStories from './MarketStories';
 
 const MainSection = () => {
     return (
-        <div className='flex gap-5'>
-            <div className='w-3/4'>
-                <h1 className='text-xl text-blue-500 font-bold bg-gray-200 p-3'>Discussion Forum</h1>
+        <div className='lg:flex'>
+            <div className='lg:w-3/4'>
+                <div className='hidden lg:block'>
+                    <h1 className='text-xl text-blue-500 font-bold bg-gray-200 p-3'>Discussion Forum</h1>
+                </div>
                 <DiscussionForum></DiscussionForum>
             </div>
 
-            <div className='w-1/4'>
-                <h1 className='text-xl text-blue-500 font-bold bg-gray-200 p-3'>Market Stories</h1>
+            <div className='lg:w-1/4'>
+                <div className='hidden lg:block'>
+                    <h1 className='text-xl text-blue-500 font-bold bg-gray-200 p-3'>Market Stories</h1>
+                </div>
                 <MarketStories></MarketStories>
             </div>
         </div>
